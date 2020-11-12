3118228b36c2d38582a1d782b06fcb4633969631
shish <anton.shyshko@logika.com.ua>
2020-11-12 16:23:56 +0200

    add permission request

--- app/src/main/java/ua/shishkoam/appcoockingbook/MainActivity.kt
+++ app/src/main/java/ua/shishkoam/appcoockingbook/MainActivity.kt
@@ -1,9 +1,15 @@
 package ua.shishkoam.appcoockingbook
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import ua.shishkoam.appcoockingbook.ui.features.RecyclerViewFragment
 import ua.shishkoam.appcoockingbook.ui.main.MainFragment
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +21,69 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, RecyclerViewFragment.newInstance(1))
                     .commitNow()
         }
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions()
+        }
+    }
+
+
+    private fun getRequiredPermissions(): Array<String?> {
+        return try {
+            val info = this.packageManager
+                .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            if (ps != null && ps.isNotEmpty()) {
+                ps
+            } else {
+                arrayOfNulls(0)
+            }
+        } catch (e: Exception) {
+            arrayOfNulls(0)
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val allNeededPermissions = ArrayList<String>()
+        for (permission in getRequiredPermissions()) {
+            permission?.let {
+                if (!isPermissionGranted(this, it)) {
+                    allNeededPermissions.add(permission)
+                }
+            }
+        }
+
+        if (allNeededPermissions.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this, allNeededPermissions.toTypedArray(), PERMISSION_REQUESTS
+            )
+        }
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.i(TAG, "Permission granted: $permission")
+            return true
+        }
+        Log.i(TAG, "Permission NOT granted: $permission")
+        return false
+    }
+
+
+    companion object {
+        private const val TAG = "ChooserActivity"
+        private const val PERMISSION_REQUESTS = 1
     }
 }
